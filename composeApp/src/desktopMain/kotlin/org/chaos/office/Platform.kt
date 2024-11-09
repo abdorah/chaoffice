832d0fd9afa92dd1ca0d5b1efb35dea705049466
Kotbi Abderrahmane <kotbymo@gmail.com>
2024-11-09 17:29:59 +0100

    inital commit: generate compose project for desktop

---
+++ composeApp/src/desktopMain/kotlin/org/chaos/office/Platform.kt
@@ -0,0 +1,7 @@
+package org.chaos.office
+
+class JVMPlatform {
+    val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
---
+++ composeApp/src/desktopMain/kotlin/org/chaos/office/main.kt
@@ -0,0 +1,13 @@
+package org.chaos.office
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "ChaOffice",
+    ) {
+        App()
+    }
+}
